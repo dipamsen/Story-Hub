f722970467e4d36d9a57e86fab0682fb7326e29f
Dipam <dipamdiptam@gmail.com>
2020-07-31 18:26:52 +0530

    Add files via upload

--- screens/write.js
+++ screens/write.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 
 export default class Write extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      author: '',
+      story: ''
+    }
+  }
   render() {
     return (
       <View style={styles.container} onPress={() => { Keyboard.dismiss() }}>
-        <TextInput style={styles.input} multiline numberOfLines={10} placeholder="Write your story here..." />
+        <TextInput style={styles.sinput} placeholder="Title" value={this.state.title} onChangeText={(title) => { this.setState({ title }) }} placeholderTextColor="black" />
+        <TextInput style={styles.sinput} placeholder="Author's Name" value={this.state.author} onChangeText={(author) => { this.setState({ author }) }} placeholderTextColor="black" />
+        <TextInput style={styles.input} multiline numberOfLines={10} placeholder="Write your story here..." value={this.state.story} onChangeText={(story) => { this.setState({ story }) }} placeholderTextColor="black" />
         <TouchableOpacity style={styles.submit} onPress={() => { Keyboard.dismiss() }}>
           <Text style={{ fontSize: 16 }}>Submit</Text>
         </TouchableOpacity>
@@ -27,7 +37,18 @@ const styles = StyleSheet.create({
     flex: 0.7,
     padding: 10,
     fontSize: 16,
-    textAlignVertical: 'top'
+    textAlignVertical: 'top',
+    margin: 4
+  },
+  sinput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: '90%',
+    fontSize: 16,
+    margin: 4,
+    maxHeight: 40,
+    // flex: 0.13
   },
   submit: {
     backgroundColor: 'lightgreen',
